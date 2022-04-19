ba03c7025d77a9cfa278d976f142b96f4276fbbc
superman312 <68647023+superman312@users.noreply.github.com>
2022-04-19 18:15:13 +0200

    test

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/output.css">
+    <link rel="stylesheet" href="css/output.css">
     <title>Document</title>
 </head>
 <body>
-    <p class="text-slate-600">banaan</p>
+    <p class=" text-red-600">banaan</p>
 </body>
 </html>
